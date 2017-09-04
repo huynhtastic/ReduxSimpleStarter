8d96ea96c302b2b9b3372e5fce74764515f14fd0
Richard L Huynh <huynhtastic@gmail.com>
2017-09-04 15:19:53 -0500

    import youtube-search; added search results of surfboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCZpDJascVg29BS80wz0roj-79_1OCIRaw'
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
