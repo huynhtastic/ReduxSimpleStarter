f1901674a3b9fa32bbb9439ed05aebd80907b501
Richard L Huynh <huynhtastic@gmail.com>
2017-09-03 19:06:08 -0500

    add onInputChange event listener to searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return <input />;
-    }
+  render() {
+    // refactored 1.16.1 return <input onChange={this.onInputChange} />;
+    // create an input with onChange property and have onChange trigger
+    // onInputChange function
+    // 1.16.1 refactor below; 1.16.2 took off parens from (event)
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+  /* refactored as part of 1.16.1
+  onInputChange(event) {
+    // event object describes what event occurred
+    // technical properties like what the text was that changed
+    console.log(event.target.value);
+  }
+  */
 }
 
 export default SearchBar;
