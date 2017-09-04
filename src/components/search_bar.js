4b81fbc69a1b9fdcb0c2cdc8296140dbd3794923
Richard L Huynh <huynhtastic@gmail.com>
2017-09-03 19:23:33 -0500

    change event listener to update state instead of output

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,7 +17,16 @@ class SearchBar extends Component {
     // create an input with onChange property and have onChange trigger
     // onInputChange function
     // 1.16.1 refactor below; 1.16.2 took off parens from (event)
-    return <input onChange={event => console.log(event.target.value)} />;
+    // 1.18.1 changed to update state instead
+    // return <input onChange={event => console.log(event.target.value)} />;
+    // => is the same as function(event) {} or a function definition
+    // bad way to set state: this.state.term = event.target.value
+    return (
+      <div>
+        <input onChange={event => this.setState(
+          { term: event.target.value })} />
+      </div>
+    );
   }
   /* refactored as part of 1.16.1
   onInputChange(event) {
