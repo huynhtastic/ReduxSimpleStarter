2c9977b0305bf65a1bfe946d0ddf8dcf507cd98e
Richard L Huynh <huynhtastic@gmail.com>
2017-09-17 18:35:40 -0500

    added clickable list items and embeded vid

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // below replaced with es6 call of ({video}) above
   //  const video = props.video;
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object"  src={imageUrl} />
